1451f490f97c47a33d434fb233cf22198dfd9f23
sumin <kim.sumin.wat2014@gmail.com>
2021-06-20 17:21:17 -0700

    removed reducer function

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,35 +1,13 @@
 import { useEffect, useReducer } from "react";
 import axios from "axios";
+import {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW,
+  reducer,
+} from "../reducers/stateReducer";
 
 export default function useApplicationData() {
-  const SET_DAY = "SET_DAY";
-  const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-  const SET_INTERVIEW = "SET_INTERVIEW";
-
-  function reducer(state, action) {
-    switch (action.type) {
-      case SET_DAY:
-        return {
-          ...state,
-          day: action.value,
-        };
-      case SET_APPLICATION_DATA:
-        return {
-          ...state,
-          ...action.value,
-        };
-      case SET_INTERVIEW: {
-        return {
-          ...state,
-          ...action.value,
-        };
-      }
-      default:
-        throw new Error(
-          `Tried to reduce with unsupported action type: ${action.type}`
-        );
-    }
-  }
   const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
